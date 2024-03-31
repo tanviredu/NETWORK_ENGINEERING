868ec7ce61258885bee05319a8ebab22f9930b23
tanviredu <tanviredu2018@gmail.com>
2024-03-31 07:16:35 +0600

    added

---
+++ PYTHON_FOR_LINUX_SYSTEM_ADMINISTRATION/2_platform_module/script.py
@@ -0,0 +1,10 @@
+import platform
+
+print(platform.python_version()) ## print installed pytohn version
+print("---------------------------")
+print(platform.machine()) ## CPU architecture
+print("---------------------------")
+print(platform.system()); ## Linux/windows/Mac
+print("---------------------------")
+print(platform.node())   ## hostname
+
